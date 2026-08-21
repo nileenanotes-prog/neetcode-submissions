d9fc7075f9ceffa38313b6d4dc50849012ff4596
nileenanotes-prog <nileena.notes@gmail.com>
2026-08-21 12:40:27 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.java
@@ -0,0 +1,77 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int i, mid;
+        char poped;
+
+        // not bracket pairs
+        if ( (s.length())%2 != 0 )
+            return false;
+
+        for (i=0; i<s.length(); i++){
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
+                stack.push(s.charAt(i));
+            else{
+                if(stack.empty())
+                    return false;
+
+                poped = stack.peek();
+
+                if ( 
+                    ( poped == '(' && s.charAt(i) == ')' ) || 
+                    ( poped == '{' && s.charAt(i) == '}' ) ||
+                    ( poped == '[' && s.charAt(i) == ']' ) 
+                ){
+                    stack.pop();
+                    continue;
+                }
+
+                else
+                    return false;
+            }
+        }
+
+        if ( (i==s.length()) && (stack.empty()) )
+            return true;
+        else
+            return false;
+
+
+        /* 3 things
+        - if only closed - check if stack empty for incoming closed }, 
+            if yes return false
+        - if only open - check if stack empty at end
+        - if combination - for each open entered, 
+            the close should match the last entered open
+
+        */
+
+
+
+
+
+
+        /*
+        mid =  (s.length())/2;
+
+        for (i=0; i<mid; i++){
+            stack.push(s.charAt(i));
+        } 
+
+        for (i=mid; i< s.length(); i++){
+            poped = stack.pop();
+            if (
+                ( poped == '(' && s.charAt(i) == ')' ) || 
+                ( poped == '{' && s.charAt(i) == '}' ) ||
+                ( poped == '[' && s.charAt(i) == ']' ) 
+                )
+                continue;
+            else
+                return false;
+                
+        } 
+
+        return true;
+        */
+    }
+}
